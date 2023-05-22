cd7c7ee224669e3361b3a31a92972c2086901886
clubatro <133589297+clubatro@users.noreply.github.com>
2023-05-21 22:12:06 -0500

    Create rpg-unreg.js
    Signed-off-by: clubatro <133589297+clubatro@users.noreply.github.com>

---
+++ plugins/rpg-unreg.js
@@ -0,0 +1,14 @@
+import { createHash } from 'crypto'
+let handler = async function (m, { args, usedPrefix, command }) {
+let idreg = `${lenguajeGB.lenguaje() == 'es' ? 'idregistro' : 'idregister'}`
+let regbot = `${lenguajeGB.lenguaje() == 'es' ? 'verificar nombre.edad' : 'verify name.age'}`
+if (!args[0]) throw lenguajeGB.smsUnreg1(usedPrefix, idreg)
+let user = global.db.data.users[m.sender]
+let sn = createHash('md5').update(m.sender).digest('hex').slice(0, 6)	
+if (args[0] !== sn) throw lenguajeGB.smsUnreg2(usedPrefix, idreg)
+user.registered = false
+m.reply(lenguajeGB.smsUnreg3(usedPrefix, regbot))
+}
+handler.command = /^anulareg|unreg(ister)?$/i
+handler.register = true
+export default handler
